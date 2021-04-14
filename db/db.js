23a67dbbde133a52fffeb775d01bb60a0194ff3a
santiagopc08 <santiagoperezcastro365@gmail.com>
2021-04-14 12:23:20 -0500

    correction on db conn

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('../models/CitaClinica');
 require('../models/Paciente');
 
-const db = mongoose.connect('mongodb+srv://audiovital:audiovital@audiovital.vkrqn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const db = mongoose.connect('mongodb://audiovital:audiovital@audiovital.vkrqn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
