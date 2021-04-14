3fa7227ec721f7608b21aa559204a2c25feec784
santiagopc08 <santiagoperezcastro365@gmail.com>
2021-04-14 14:18:51 -0500

    keep trying

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,6 @@ const citasRoutes = require("./citasRoutes")
 const router = express.Router();
 
 router.use("/", pacienteRoutes);
-router.use("/", maintenanceRoutes);
 router.use("/", citasRoutes);
 //
 module.exports = router;
