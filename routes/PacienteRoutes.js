04324d0d31c4c0f9aae4331f80219c2d1512c739
santiagopc08 <santiagoperezcastro365@gmail.com>
2021-04-14 09:39:06 -0500

    added some stuff

--- routes/PacienteRoutes.js
+++ routes/PacienteRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const paciente = require('../Controllers/PacienteController')
+const paciente = require('../Controllers/PacienteController.js')
 
 const router = express.Router()
 
