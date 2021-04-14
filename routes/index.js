bcdf5257aed6f2da6adf25c015e0d74b3154d39d
santiagopc08 <santiagoperezcastro365@gmail.com>
2021-04-14 14:35:34 -0500

    working 2

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 
 const pacienteRoutes = require("./PacienteRoutes");
-const maintenanceRoutes = require("./MaintenanceRoutes")
-const citasRoutes = require("./citasRoutes")
+const citasRoutes = require("./citasRoutes");
 
 const router = express.Router();
 
